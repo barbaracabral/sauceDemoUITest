55943f417b41faf4ba9fc0c22b6f78f9757a1168
IhGori <igor.vinicius@discente.univasf.edu.br>
2024-08-03 15:22:46 -0300

    feat: Atualiza yml e config do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  videoUploadOnPasses: true,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
